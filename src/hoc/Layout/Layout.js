65538b050723a7343c8b07bfe43b3f3aceac5eb7
LukasKaselionis <l.kaselionis@gmail.com>
2020-06-17 12:11:20 +0300

    Created Layout with hoc

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Auxiliary from '../Auxiliary/Auxiliary';
+import classes from './Layout.css';
+
+const layout = () => {
+    return (
+        <Auxiliary>
+            <div className={classes.Layout}>
+                <span>TESTING</span>
+            </div>
+        </Auxiliary>
+    )
+}
+
+export default layout;
